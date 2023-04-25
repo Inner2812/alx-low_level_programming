3ed01bb49dbafa6d260765cd2387405a71914133
Inner2812 <nyongwanaina@gmail.com>
2023-04-25 07:59:32 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *
+ * main- Prints alphabets in reverse
+ * 
+ * return is always 0
+ */
+int main(void)
+{ 
+	char ch;
+	for (ch = 'z'; ch >='a'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar("\n")
+	return (0)
+
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+/**
+ *
+ * main prints numbers from 0 to 9 and letters from a to f
+ *
+ * return is always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	for (i = 48; i < 58; i++)
+	{ 
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+	return (0)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
